a83017fc0463c44ff3097fd9d9fa873a054200bc
lechwolowski <lech.wolowski@gmail.com>
2020-03-30 21:09:56 +0200

    added dump

--- helpler.py
+++ helpler.py
@@ -2,6 +2,9 @@ from config import MAP
 from models.Road import Road
 from models.Grass import Grass
 from models.House import House
+from models.Trash_Glass import Trash_Glass
+from models.Trash_Paper import Trash_Paper
+from models.Trash_Plastic import Trash_Plastic
 
 
 def Render_Element(x, y):
@@ -12,5 +15,9 @@ def Render_Element(x, y):
         return Grass(x, y)
     elif item == "House":
         return House(x, y)
-    elif item == "Factory":
-        pass
+    elif item == "Glass":
+        return Trash_Glass(x, y)
+    elif item == "Paper":
+        return Trash_Paper(x, y)
+    elif item == "Plastic":
+        return Trash_Plastic(x, y)
--- models/Trash_Plastic.py
+++ models/Trash_Plastic.py
@@ -2,10 +2,10 @@ import pygame
 from config import CELL_SIZE
 
 
-class Dump (pygame.sprite.Sprite):
+class Trash_Plastic (pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.rect = pygame.Rect(
             x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
         self.image = pygame.transform.scale(pygame.image.load(
-            "Resources/Images/dump.jpg"), (CELL_SIZE, CELL_SIZE))
+            "Resources/Images/trash-plastic.png"), (CELL_SIZE, CELL_SIZE))
