d5b271ceb03e0c437d6ce92b8b629d16d76c69a6
lechwolowski <lech.wolowski@gmail.com>
2020-03-31 22:39:14 +0200

    number of trash in house number

--- models/House.py
+++ models/House.py
@@ -1,5 +1,8 @@
 import pygame
 from config import CELL_SIZE
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 
 class House (pygame.sprite.Sprite):
@@ -11,12 +14,21 @@ class House (pygame.sprite.Sprite):
         self.plastic = 5
         self.rect = pygame.Rect(
             x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        self.image = pygame.transform.scale(pygame.image.load(
-            "Resources/Images/house.jpg"), (CELL_SIZE, CELL_SIZE))
+        self.img_update()
+
+    def img_update(self):
+        img = Image.open("Resources/Images/house.jpg")
+        img = img.resize((CELL_SIZE, CELL_SIZE))
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("FiraCode-Bold.otf", 16)
+        draw.text((20, 20), str(self.glass), (0, 0, 0), font=font)
+        data, size, mode = img.tobytes(), img.size, img.mode
+        self.image = pygame.image.frombuffer(data, size, mode)
 
     def get_mixed(self):
         if self.mixed > 0:
             self.mixed -= 1
+            self.img_update()
             return True
         else:
             return False
@@ -24,6 +36,7 @@ class House (pygame.sprite.Sprite):
     def get_paper(self):
         if self.paper > 0:
             self.paper -= 1
+            self.img_update()
             return True
         else:
             return False
@@ -31,6 +44,7 @@ class House (pygame.sprite.Sprite):
     def get_glass(self):
         if self.glass > 0:
             self.glass -= 1
+            self.img_update()
             return True
         else:
             return False
@@ -38,6 +52,7 @@ class House (pygame.sprite.Sprite):
     def get_plastic(self):
         if self.plastic > 0:
             self.plastic -= 1
+            self.img_update()
             return True
         else:
             return False
