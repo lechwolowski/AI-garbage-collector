24f2cc78b52563ac2c1271b2df6b34ccc2500236
lechwolowski <lech.wolowski@gmail.com>
2020-05-05 00:21:05 +0200

    fix typeo

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ clock = pygame.time.Clock()
 
 # know = Knowledge(draw_items, gc)
 
-model = load_model(os.path.join('trained_models', 'working_one_trash.model'))
+model = load_model(os.path.join('trained_models', 'working_one_house.model'))
 
 # Game Loop
 run_a = False
