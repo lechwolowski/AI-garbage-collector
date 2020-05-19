bbe37cc0f56c92b1232cf4a85792c3fce8a4164f
Maciej Scigacz <macsci1999@gmail.com>
2020-05-19 15:09:23 +0200

    little fix

--- Tree/decision_tree.py
+++ Tree/decision_tree.py
@@ -2,6 +2,6 @@ from sklearn import tree
 
 
 def make_tree(maps, actions):
-    clf = tree.DecisionTreeClassifier(criterion="entropy")
+    clf = tree.DecisionTreeClassifier()
     clf = clf.fit(maps, actions)
     return clf
